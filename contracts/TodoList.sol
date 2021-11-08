78b311caec9bb48343daf8fe753d7b285020fe2c
mvelosos <mfvelosos@gmail.com>
2021-11-08 00:53:16 -0300

    create index, js and test files

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.8.9;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  constructor() {
+    createTask("Etherum Blockchain Dapp");
+  }
+
+  function createTask(string memory _content)  public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
